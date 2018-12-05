4a4f6bbfd441837d5d850aaf4f344c6083f24c73
Aszmel <aszmel@o2.pl>
2018-12-05 22:43:05 +0100

    some fun with styled

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,49 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import styled, { createGlobalStyle } from 'styled-components'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    color: ghostwhite;
+  }
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background: palevioletred;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
+    'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+
+  ::before {
+    content: '';
+    width: 50px;
+    height: 50px;
+    display: block;
+    background: yellow;
+  }
+`
+
+const H1 = styled.h1`
+  font-size: ${({ isBig }) => (isBig ? '5em' : '3em')};
+`
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <>
+    <GlobalStyle />
+    <StyledWrapper>
+      <H1 isBig>Hi people</H1>
+      <H1>This is cool page made with Gatsby!</H1>
+    </StyledWrapper>
+  </>
 )
 
 export default IndexPage
